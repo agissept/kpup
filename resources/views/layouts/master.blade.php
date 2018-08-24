710ff0dccafdd82478d43af9af01b9ce566bf31a
Agista Septiyanto <agis.septiyanto@gmail.com>
2018-08-24 07:51:00 +0700

    Add cdn framework

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,18 +7,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     
-    <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.css')}}">
-    <link rel="stylesheet" href="{{asset('bower_components/font-awesome/web-fonts-with-css/css/fontawesome-all.css')}}">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('style.css')}}">
-    <link rel="stylesheet" href="{{asset('bower_components/css-hamburgers/dist/hamburgers.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hamburgers/0.9.3/hamburgers.min.css">
+
+
+    {{-- <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('bower_components/font-awesome/web-fonts-with-css/css/fontawesome-all.css')}}">
+    
+    <link rel="stylesheet" href="{{asset('bower_components/css-hamburgers/dist/hamburgers.css')}}"> --}}
 </head>
 
 <body>
     @yield('content')
 
-    <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    {{-- <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
     <script src="{{asset('bower_components/popper.js/dist/popper.min.js')}}"></script>
-    <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.js')}}"></script>
+    <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.js')}}"></script> --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
     <script src="{{asset('main.js')}}"></script>
 
